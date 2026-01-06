0a73783791efbca18c02323ee92d992178148325
dhruvibosamiya-10 <dhruvibosamiya991@gmail.com>
2026-01-06 08:14:02 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,28 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello world")
+hello world
+x=5
+print(x)
+5
+y=20.5
+print(y)
+20.5
+z=5j
+print(z)
+5j
+a=["apple","banana","cherry"]
+print(a)
+['apple', 'banana', 'cherry']
+b=("apple","banana","cherry")
+print(b)
+('apple', 'banana', 'cherry')
+c=false
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    c=false
+NameError: name 'false' is not defined. Did you mean: 'False'?
+
+c=False
+print(c)
+False
